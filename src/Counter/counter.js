0dd75ff06a72de7faa1574de70ddd7d94f89cca1
alescandruu <alex.daniliuc14@yahoo.com>
2022-08-19 15:51:20 +0300

    UI changes

--- src/Counter/counter.js
+++ src/Counter/counter.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import useStoreState from '../StateManagement/customHook';
+import "../Counter/Counter.css"
 
 function Counter() {
   const [ counter, setCounter ] = useStoreState(null);
